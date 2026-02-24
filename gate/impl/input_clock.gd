5236d0a8e32f92a3fe097442299b7ad12c1ea52a
leon705 <leon.jhnig@googlemail.com>
2026-02-24 23:39:48 +0100

    clock: fixed behavior at 0Hz

--- gate/impl/input_clock.gd
+++ gate/impl/input_clock.gd
@@ -6,12 +6,19 @@ var interval: float;
 var timer: float = 0.0;
 
 func _on_changed(value: float) -> void:
-	interval = 1.0 / value;
+	interval = 1.0 / value if value > 0.0 else 0.0;
+	
+	if interval == 0.0:
+		output_state = false;
+		_propagate_signal();
 
 func _compute(_inputs: Array[bool]) -> bool:
 	return output_state;
 
 func _process(delta: float) -> void:
+	if interval == 0.0:
+		return;
+
 	timer += delta;
 	if timer >= interval:
 		output_state = !output_state;
@@ -23,6 +30,7 @@ func _get_slot_ui_component(_idx: int) -> Control:
 	spin_box.set_value_no_signal(0.0);
 	spin_box.max_value = 20.0;
 	spin_box.suffix = "Hz";
+	spin_box.custom_minimum_size.x = 75;
 	spin_box.size_flags_horizontal = Control.SIZE_EXPAND_FILL;
 	spin_box.mouse_filter = MOUSE_FILTER_STOP;
 	spin_box.value_changed.connect(_on_changed);
