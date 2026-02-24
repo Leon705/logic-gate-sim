86583671c1f3c4bd83c794c8886c5de77f493296
leon705 <leon.jhnig@googlemail.com>
2026-02-24 22:54:33 +0100

    updated to latest version

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1771969323|13279363242';
+const CACHE_VERSION = '1771970048|14003952154';
 /** @type {string} */
 const CACHE_PREFIX = 'LogicGateSim-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
